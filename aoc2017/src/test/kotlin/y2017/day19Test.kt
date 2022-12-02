c476c8dd5e56902e049b1862f4efc6a1863c52a4
Art Doler <art.doler@aviture.us.com>
2022-12-02 08:36:16 -0600

    Fix 2017 tests.

--- aoc2017/src/test/kotlin/y2017/day19Test.kt
+++ aoc2017/src/test/kotlin/y2017/day19Test.kt
@@ -1,7 +1,7 @@
 package y2017
 
-import org.junit.Assert.assertEquals
-import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 import java.io.File
 
 class day19Test {
