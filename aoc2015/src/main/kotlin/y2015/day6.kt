64b1de727c7a5986e83b9e4c00b47a965c9ed35d
Arthur Doler <arthurdoler@gmail.com>
2024-12-02 13:06:43 -0600

    AOC 2015 Day 6. Got the urge to get a few more stars...

--- aoc2015/src/main/kotlin/y2015/day6.kt
+++ aoc2015/src/main/kotlin/y2015/day6.kt
@@ -1,21 +1,61 @@
 package y2015
 
+val day6regexp = "(turn on|turn off|toggle) (\\d+,\\d+) through (\\d+,\\d+)".toRegex();
+
 fun day6(input: List<String>): Int {
-    var lights = List(1000) { List(1000) { false } }
+    val lights = MutableList(1000) { MutableList(1000) { 0 } }
 
+    for (instruction in input) {
+        val match = day6regexp.matchEntire(instruction) ?: return -1;
 
-    return lights.map { list -> (list.filter { b -> b }.size) }.sum()
+        val coord1 = match.groupValues[2].split(",").map { it.toInt() };
+        val coord2 = match.groupValues[3].split(",").map { it.toInt() };
+
+        val operation = match.groupValues[1];
+        for (x in coord1[0]..coord2[0]) {
+            for (y in coord1[1]..coord2[1]) {
+                if (operation == "turn on") {
+                    lights[x][y] = 1;
+                } else if (operation == "turn off") {
+                    lights[x][y] = 0;
+                } else {
+                    lights[x][y] = if (lights[x][y] == 1) 0 else 1;
+                }
+            }
+        }
+
+    }
+
+    return lights.sumOf { list -> list.sum() }
 }
 
 
-//fun day6Second(input: List<String>): Int {
-//    return input.map { s -> isNiceSecond(s) }.filter { b -> b }.size
-//}
-//
-//val twoLetterRepeatRegex = "([a-z]{2}).*\\1".toRegex()
-//val oneLetterRepeatRegex = "([a-z])[a-z]\\1".toRegex()
-//fun isNiceSecond(input: String): Boolean {
-//    return twoLetterRepeatRegex.containsMatchIn(input) &&
-//            oneLetterRepeatRegex.containsMatchIn(input)
-//
-//}
+fun day6Second(input: List<String>): Int {
+    val lights = MutableList(1000) { MutableList(1000) { 0 } }
+
+    for (instruction in input) {
+        val match = day6regexp.matchEntire(instruction) ?: return -1;
+
+        val coord1 = match.groupValues[2].split(",").map { it.toInt() };
+        val coord2 = match.groupValues[3].split(",").map { it.toInt() };
+
+        val operation = match.groupValues[1];
+        for (x in coord1[0]..coord2[0]) {
+            for (y in coord1[1]..coord2[1]) {
+                if (operation == "turn on") {
+                    lights[x][y] += 1;
+                } else if (operation == "turn off") {
+                    lights[x][y] -= 1;
+                    if (lights[x][y] < 0) {
+                        lights[x][y] = 0;
+                    }
+                } else {
+                    lights[x][y] += 2;
+                }
+            }
+        }
+
+    }
+
+    return lights.sumOf { list -> list.sum() }
+}
--- aoc2015/src/test/kotlin/y2015/day6Test.kt
+++ aoc2015/src/test/kotlin/y2015/day6Test.kt
@@ -3,6 +3,7 @@ package y2015.src.test.kotlin.y2015
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import y2015.day6
+import y2015.day6Second
 import java.io.File
 
 class day6Test {
@@ -15,53 +16,28 @@ class day6Test {
         assertEquals(1_000_000, day6(listOf("turn on 0,0 through 999,999")))
     }
 
-//    @Test
-//    fun test2() {
-//        assertEquals(true, isNice("aaa"))
-//    }
-//
-//    @Test
-//    fun test3() {
-//        assertEquals(false, isNice("jchzalrnumimnmhp"))
-//    }
-//
-//    @Test
-//    fun test4() {
-//        assertEquals(false, isNice("haegwjzuvuyypxyu"))
-//    }
-//
-//    @Test
-//    fun test5() {
-//        assertEquals(false, isNice("dvszwmarrgswjxmb"))
-//    }
-//
-//    @Test
-//    fun testActual() {
-//        assertEquals(258, day5(parseFile("y2015/day05ActualInput.txt")))
-//    }
-//
-//    @Test
-//    fun test1Second() {
-//        assertEquals(true, isNiceSecond("qjhvhtzxzqqjkmpb"))
-//    }
-//
-//    @Test
-//    fun test2Second() {
-//        assertEquals(true, isNiceSecond("xxyxx"))
-//    }
-//
-//    @Test
-//    fun test3Second() {
-//        assertEquals(false, isNiceSecond("uurcxstgmygtbstg"))
-//    }
-//
-//    @Test
-//    fun test4Second() {
-//        assertEquals(false, isNiceSecond("ieodomkazucvgmuy"))
-//    }
-//
-//    @Test
-//    fun testActualSecond() {
-//        assertEquals(53, day5Second(parseFile("y2015/day05ActualInput.txt")))
-//    }
+    @Test
+    fun test2() {
+        assertEquals(999_996, day6(listOf("turn on 0,0 through 999,999","turn off 499,499 through 500,500")))
+    }
+
+    @Test
+    fun testActual() {
+        assertEquals(569_999, day6(parseFile("y2015/day06ActualInput.txt")))
+    }
+
+    @Test
+    fun test1Second() {
+        assertEquals(1, day6Second(listOf("turn on 0,0 through 0,0")))
+    }
+
+    @Test
+    fun test2Second() {
+        assertEquals(2_000_000, day6Second(listOf("toggle 0,0 through 999,999")))
+    }
+
+    @Test
+    fun testActualSecond() {
+    assertEquals(17_836_115, day6Second(parseFile("y2015/day06ActualInput.txt")))
+    }
 }
